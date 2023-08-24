b84be9878b688f8fac399a938a798587c1f67973
MargaritaSukhova <margarytasu@gmail.com>
2023-08-24 23:11:07 +0300

    Update MainPageTitle.styled.jsx

--- src/components/MainPageTitle/MainPageTitle.styled.jsx
+++ src/components/MainPageTitle/MainPageTitle.styled.jsx
@@ -3,10 +3,18 @@ import styled from "@emotion/styled";
 const MainTitle = styled.h2`
 	color: #f3f3f3;
 	font-family: Manrope;
-	font-size: 32px;
 	font-style: normal;
 	font-weight: 600;
+	font-size: 32px;
 	line-height: ${38 / 32}px;
+	@media (min-width: 768px) {
+		font-size: 56px;
+		line-height: ${60 / 56}px;
+	}
+	@media (min-width: 1440px) {
+		font-size: 64px;
+		line-height: ${68 / 64}px;
+	}
 `;
 
 export default MainTitle;
