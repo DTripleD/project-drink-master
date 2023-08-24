9d53fa17e6ad246540bc8fd8830195185cae3d24
MargaritaSukhova <margarytasu@gmail.com>
2023-08-24 22:46:03 +0300

    main title

---
+++ src/components/MainPageTitle/MainPageTitle.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+const MainTitle = styled.h2`
+	color: #f3f3f3;
+	font-family: Manrope;
+	font-size: 32px;
+	font-style: normal;
+	font-weight: 600;
+	line-height: ${38 / 32}px;
+`;
+
+export default MainTitle;
