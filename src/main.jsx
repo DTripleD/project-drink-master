e510e759419e219b866dfc9a01f001dee517dd91
Danil Deineka <tripledpadik@gmail.com>
2023-08-24 03:04:53 +0300

    Add files for components

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,31 @@ import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = {
+  colors: {
+    black: "#0A0A11",
+    blue: "#161F37",
+    grey: "#434D67",
+    lightBlue: "#4070CD",
+    green: "#BCE6D2",
+    white: "#F3F3F3",
+  },
+  fonst: {},
+  breakpoint: {
+    mobile: 375,
+    tablet: 768,
+    desktop: 1440,
+  },
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
