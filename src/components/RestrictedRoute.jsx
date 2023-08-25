e66a74c3e737b93d912b149df96103f144f2628d
Danil Deineka <tripledpadik@gmail.com>
2023-08-25 17:50:47 +0300

    Base change

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,15 +1,15 @@
-import { Navigate } from "react-router-dom"; 
-import PropTypes from "prop-types"; 
-// import { selectIsLoggedIn } from "redux/auth/selectors"; 
-// import { useSelector } from "react-redux"; 
- 
-export const RestrictedRoute = ({ component: Component, redirectTo = "/" }) => { 
-  //   const isLoggedIn = useSelector(selectIsLoggedIn); 
-  const isLoggedIn = false; 
-  return isLoggedIn ? <Navigate to={redirectTo} /> : Component; 
-}; 
- 
-RestrictedRoute.propTypes = { 
-  component: PropTypes.object.isRequired, 
-  redirectTo: PropTypes.string, 
+import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
+// import { selectIsLoggedIn } from "redux/auth/selectors";
+// import { useSelector } from "react-redux";
+
+export const RestrictedRoute = ({ component: Component, redirectTo = "/" }) => {
+  //   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isLoggedIn = true;
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
+
+RestrictedRoute.propTypes = {
+  component: PropTypes.object.isRequired,
+  redirectTo: PropTypes.string,
 };
