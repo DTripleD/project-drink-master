ff5677b8919c88c42f147f1f3e7e6cd501419c33
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-08-25 23:57:02 +0300

    add logo

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,28 +1,27 @@
 // import { useDispatch, useSelector } from "react-redux";
 // import { logOut } from "redux/auth/operations";
 // import { selectUser } from "redux/auth/selectors";
-import { MenuWrapper, UserName } from "./UserMenu.styled";
+import { MenuWrapper, UserName, UserPhoto } from "./UserMenu.styled";
 // import { Button } from "@mui/material";
 
 const UserMenu = () => {
-  //   const user = useSelector(selectUser);
+	//   const user = useSelector(selectUser);
 
-  //   const dispatch = useDispatch();
+	//   const dispatch = useDispatch();
 
-  //   const handleLogout = (event) => {
-  //     event.preventDefault();
-  //     dispatch(logOut());
-  //   };
+	//   const handleLogout = (event) => {
+	//     event.preventDefault();
+	//     dispatch(logOut());
+	//   };
 
-  return (
-    <MenuWrapper>
-      <UserName>Hello, Team</UserName>
-      {/* <Button variant="contained" color="error">
-        Log Out
-      </Button> */}
-      <button>Log out</button>
-    </MenuWrapper>
-  );
+	return (
+		<MenuWrapper>
+			<UserPhoto>
+				<img src="#" alt="" />
+			</UserPhoto>
+			<UserName>hello</UserName>
+		</MenuWrapper>
+	);
 };
 
 export default UserMenu;
--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -1,14 +1,21 @@
 import styled from "@emotion/styled";
 
+export const UserPhoto = styled.div`
+	width: 32px;
+	height: 32px;
+	border-radius: 50%;
+	background-color: green;
+`;
+
 export const UserName = styled.p`
-  font-size: 20px;
-  margin: 0;
+	font-size: 14px;
+	margin: 0;
 `;
 
 export const MenuWrapper = styled.div`
-  display: flex;
+	display: flex;
 
-  align-items: center;
+	align-items: center;
 
-  gap: 15px;
+	gap: 8px;
 `;
