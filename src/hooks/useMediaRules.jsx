8a1b7ca59ff6c16109dedea6fad9a25cc39d6655
jane05070 <jane05070@gmail.com>
2023-08-25 18:28:33 +0300

    footer

---
+++ src/hooks/useMediaRules.jsx
@@ -0,0 +1,14 @@
+import { useMediaQuery } from 'react-responsive';
+
+const useMediaRules = () => {
+    const isMobile = useMediaQuery({ query: '(max-width: 767.98px)' });
+    const isTablet = useMediaQuery({
+        query: '(min-width: 768px) and (max-width: 1439.98px)',
+    });
+    const isDesktop = useMediaQuery({ query: '(min-width: 1440px)' });
+    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
+
+    return { isMobile, isTablet, isDesktop, isRetina };
+};
+
+export default useMediaRules;
