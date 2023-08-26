a77e51f008ec4a463719e816e6c35a36eb51d085
MargaritaSukhova <margarytasu@gmail.com>
2023-08-26 12:17:10 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,29 +5,37 @@ import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@emotion/react";
 
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+
 const theme = {
-  colors: {
-    black: "#0A0A11",
-    blue: "#161F37",
-    grey: "#434D67",
-    lightBlue: "#4070CD",
-    green: "#BCE6D2",
-    white: "#F3F3F3",
-  },
-  fonst: {},
-  breakpoint: {
-    mobile: 375,
-    tablet: 768,
-    desktop: 1440,
-  },
+	colors: {
+		black: "#0A0A11",
+		blue: "#161F37",
+		grey: "#434D67",
+		lightBlue: "#4070CD",
+		green: "#BCE6D2",
+		white: "#F3F3F3",
+	},
+	fonst: {},
+	breakpoint: {
+		mobile: 375,
+		tablet: 768,
+		desktop: 1440,
+	},
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Router basename="/project-drink-master">
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </Router>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<Router basename="/project-drink-master">
+					<ThemeProvider theme={theme}>
+						<App />
+					</ThemeProvider>
+				</Router>
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>
 );
