c7976d2ffbdf496f34dcf99febe0128eca02d8e1
andreas7500 <andreas2875@gmail.com>
2023-08-27 13:35:55 +0300

    delete coomit

--- src/components/MainPageTitle/MainPageTitle.jsx
+++ src/components/MainPageTitle/MainPageTitle.jsx
@@ -1,18 +1,3 @@
-// import PropTypes from "prop-types";
-// import { MainTitle } from "./MainPageTitle.styled";
-
-// export const MainPageTitle = ({ title }) => {
-//   return (
-//     <div>
-//       <MainTitle>{title}</MainTitle>
-//     </div>
-//   );
-// };
-
-// MainPageTitle.propTypes = {
-//   title: PropTypes.string.isRequired,
-// };
-
 import PropTypes from "prop-types";
 import MainTitle from "./MainPageTitle.styled";
 
--- src/components/MainPageTitle/MainPageTitle.styled.jsx
+++ src/components/MainPageTitle/MainPageTitle.styled.jsx
@@ -1,24 +1,5 @@
 import styled from "@emotion/styled";
 
-// export const MainTitle = styled.h2`
-//   color: #f3f3f3;
-//   font-family: Manrope;
-//   font-style: normal;
-//   font-weight: 600;
-//   font-size: 32px;
-//   line-height: ${38 / 32}px;
-//   @media (min-width: 768px) {
-//     font-size: 56px;
-//     line-height: ${60 / 56}px;
-//   }
-//   @media (min-width: 1440px) {
-//     font-size: 64px;
-//     line-height: ${68 / 64}px;
-//   }
-// `;
-
-// export default MainTitle;
-
 const MainTitle = styled.h1`
   color: #f3f3f3;
   font-size: 32px;
