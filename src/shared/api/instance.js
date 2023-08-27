2ae2dd53d003a482cf6a11d1d48b84857a0db83e
Danil Deineka <tripledpadik@gmail.com>
2023-08-27 13:46:41 +0300

    Update baseURL

--- src/shared/api/instance.js
+++ src/shared/api/instance.js
@@ -3,8 +3,8 @@ import axios from "axios";
 // const { VITE_API_URL } = process.env;
 
 const instance = axios.create({
-	// baseURL: VITE_API_URL,
-	baseURL: "http://localhost:8000",
+  // baseURL: VITE_API_URL,
+  baseURL: "https://drink-master-backend.onrender.com",
 });
 
 export default instance;
