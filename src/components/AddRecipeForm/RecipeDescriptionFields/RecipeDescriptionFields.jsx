20dbeeee2c0d1d2e44e83537f270b4b91513532a
MargaritaSukhova <margarytasu@gmail.com>
2023-08-27 23:38:46 +0300

    added photo preview

--- src/components/AddRecipeForm/RecipeDescriptionFields/RecipeDescriptionFields.jsx
+++ src/components/AddRecipeForm/RecipeDescriptionFields/RecipeDescriptionFields.jsx
@@ -8,6 +8,8 @@ import {
 } from "../../../shared/api/addRecipePageQuery";
 import {
   Container,
+  AddImageButtonContainer,
+  Image,
   StyledImageInput,
   AddImageButton,
   AddImageTitle,
@@ -48,73 +50,89 @@ const RecipeDescriptionFields = ({ register, control }) => {
     label: glass,
   }));
 
+  const handleUploadClick = (event) => {
+      console.log(event.target.files[0]);
+			const file = event.target.files[0];
+			const reader = new FileReader();
+			if (file) {
+				reader.readAsDataURL(file);
+				reader.onloadend = function (e) {
+					setImage(reader.result);
+					// onChange({ img: event.target.files[0] });
+				};
+			}
+    };
+  
   return (
-    <Container>
-      <StyledImageInput>
-        <input
-          name="drinkThumb"
-          id="drinkThumb"
-          type="file"
-          placeholder="Add image"
-          hidden
-        />
-        <AddImageButton htmlFor="drinkThumb">
-          <Plus style={{ width: 28, height: 28 }} />
-        </AddImageButton>
-        <AddImageTitle>Add image</AddImageTitle>
-      </StyledImageInput>
-      <InputContainer>
-        <StyledInput
-          {...register("drink")}
-          type="text"
-          placeholder="Enter item title"
-        />
-        <StyledInput
-          {...register("description")}
-          type="text"
-          placeholder="Enter about recipe"
-        />
-        <SelectContainer>
-          <StyledInput disabled={true} type="text" placeholder="Category" />
-          <Controller
-            control={control}
-            name="category"
-            render={({ field: { onChange, value } }) => (
-              <StyledSelect
-                // {...field}
-                defaultValue={optionCategories[1]}
-                options={optionCategories}
-                value={value}
-                onChange={onChange}
-                placeholder="Cocktail"
-              
-                classNamePrefix={"select"}
-              />
-            )}
-          />
-        </SelectContainer>
-        <SelectContainer>
-          <StyledInput disabled={true} type="text" placeholder="Glass" />
-          <Controller
-            control={control}
-            name="glass"
-            render={({ field: { onChange, value } }) => (
-              <StyledSelect
-                // {...field}
-                defaultValue={glasses[1]}
-                options={optionGlasses}
-                value={value}
-                onChange={onChange}
-                placeholder="Highball glass"
-              
-                classNamePrefix={"select"}
-              />
-            )}
-          />
-        </SelectContainer>
-      </InputContainer>
-    </Container>
-  );
+		<Container>
+			<StyledImageInput>
+				{image && <Image src={image} alt="photo" />}
+				<input
+					name="drinkThumb"
+					id="drinkThumb"
+					type="file"
+					placeholder="Add image"
+					onChange={handleUploadClick}
+					hidden
+				/>
+				<AddImageButtonContainer>
+					<AddImageButton htmlFor="drinkThumb">
+						<Plus style={{ width: 28, height: 28 }} />
+					</AddImageButton>
+					<AddImageTitle>Add image</AddImageTitle>
+				</AddImageButtonContainer>
+			</StyledImageInput>
+
+			<InputContainer>
+				<StyledInput
+					{...register("drink")}
+					type="text"
+					placeholder="Enter item title"
+				/>
+				<StyledInput
+					{...register("description")}
+					type="text"
+					placeholder="Enter about recipe"
+				/>
+				<SelectContainer>
+					<StyledInput disabled={true} type="text" placeholder="Category" />
+					<Controller
+						control={control}
+						name="category"
+						render={({ field: { onChange, value } }) => (
+							<StyledSelect
+								// {...field}
+								defaultValue={optionCategories[1]}
+								options={optionCategories}
+								value={value}
+								onChange={onChange}
+								placeholder="Cocktail"
+								classNamePrefix={"select"}
+							/>
+						)}
+					/>
+				</SelectContainer>
+				<SelectContainer>
+					<StyledInput disabled={true} type="text" placeholder="Glass" />
+					<Controller
+						control={control}
+						name="glass"
+						render={({ field: { onChange, value } }) => (
+							<StyledSelect
+								// {...field}
+								defaultValue={glasses[1]}
+								options={optionGlasses}
+								value={value}
+								onChange={onChange}
+								placeholder="Highball glass"
+								classNamePrefix={"select"}
+							/>
+						)}
+					/>
+				</SelectContainer>
+			</InputContainer>
+		</Container>
+	);
 };
 
 RecipeDescriptionFields.propTypes = {
