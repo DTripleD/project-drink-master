80ac27347acc3b307aa7e79f040ee8cea0987489
MargaritaSukhova <margarytasu@gmail.com>
2023-08-28 23:51:14 +0300

    validation + formData

--- src/components/AddRecipeForm/RecipeDescriptionFields/RecipeDescriptionFields.jsx
+++ src/components/AddRecipeForm/RecipeDescriptionFields/RecipeDescriptionFields.jsx
@@ -3,72 +3,80 @@ import PropTypes from "prop-types";
 import Select from "react-select";
 import { Controller } from "react-hook-form";
 import {
-  getCategoriesList,
-  getGlassesList,
+	getCategoriesList,
+	getGlassesList,
 } from "../../../shared/api/addRecipePageQuery";
 import {
-  Container,
-  AddImageButtonContainer,
-  Image,
-  StyledImageInput,
-  AddImageButton,
-  AddImageTitle,
-  InputContainer,
-  StyledInput,
-  SelectContainer,
-  StyledSelect,
+	Container,
+	AddImageButtonContainer,
+	Image,
+	StyledImageInput,
+	AddImageButton,
+	AddImageTitle,
+	InputContainer,
+	StyledInput,
+	SelectContainer,
+	StyledSelect,
 } from "./RecipeDescriptionFields.styled";
 import { ReactComponent as Plus } from "../../../images/svg/add-photo.svg";
 
-const RecipeDescriptionFields = ({ register, control }) => {
-  const [image, setImage] = useState(null);
-  const [categories, setCategories] = useState([]);
-  const [glasses, setGlasses] = useState([]);
+const RecipeDescriptionFields = ({ getFile, register, control }) => {
+	const [image, setImage] = useState(null);
+	const [categories, setCategories] = useState([]);
+	const [glasses, setGlasses] = useState([]);
 
-  useEffect(() => {
-    const getCategories = async () => {
-      const result = await getCategoriesList();
-      setCategories(result);
-    };
+	useEffect(() => {
+		const getCategories = async () => {
+			const result = await getCategoriesList();
+			setCategories(result);
+		};
 
-    const getGlasses = async () => {
-      const result = await getGlassesList();
-      setGlasses(result);
-    };
+		const getGlasses = async () => {
+			const result = await getGlassesList();
+			setGlasses(result);
+		};
 
-    getCategories();
-    getGlasses();
-  }, []);
+		getCategories();
+		getGlasses();
+	}, []);
 
-  const optionCategories = categories.map((category) => ({
-    value: category.toLowerCase(),
-    label: category,
-  }));
+	const optionCategories = categories.map((category) => ({
+		value: category.toLowerCase(),
+		label: category,
+	}));
 
-  const optionGlasses = glasses.map((glass) => ({
-    value: glass.toLowerCase(),
-    label: glass,
-  }));
+	const optionGlasses = glasses.map((glass) => ({
+		value: glass.toLowerCase(),
+		label: glass,
+	}));
 
-  const handleUploadClick = (event) => {
-      console.log(event.target.files[0]);
-			const file = event.target.files[0];
-			const reader = new FileReader();
-			if (file) {
-				reader.readAsDataURL(file);
-				reader.onloadend = function (e) {
-					setImage(reader.result);
-					// onChange({ img: event.target.files[0] });
-				};
-			}
-    };
-  
-  return (
+	const handleUploadClick = (event) => {
+
+		const file = event.target.files[0];
+	
+		const reader = new FileReader();
+		if (file) {
+			reader.readAsDataURL(file);
+			reader.onloadend = () => {
+				setImage(reader.result);
+				getFile(file);
+			};
+		}
+	};
+
+	const getValue = (value, options) => {
+		value ? options.find( option => option.value===value) : ''
+	}
+
+	return (
 		<Container>
 			<StyledImageInput>
 				{image && <Image src={image} alt="photo" />}
 				<input
-					name="drinkThumb"
+					{...register("drinkThumb", {
+						required: { value: true, message: "Please add a photo" },
+					})}
+					// name="drinkThumb"
 					id="drinkThumb"
 					type="file"
 					placeholder="Add image"
@@ -82,15 +90,21 @@ const RecipeDescriptionFields = ({ register, control }) => {
 					<AddImageTitle>Add image</AddImageTitle>
 				</AddImageButtonContainer>
 			</StyledImageInput>
-
 			<InputContainer>
 				<StyledInput
-					{...register("drink")}
+					{...register("drink", {
+						required: { value: true, message: "Please fill the title field" },
+					})}
 					type="text"
 					placeholder="Enter item title"
 				/>
 				<StyledInput
-					{...register("description")}
+					{...register("description", {
+						required: {
+							value: true,
+							message: "Please fill the description field",
+						},
+					})}
 					type="text"
 					placeholder="Enter about recipe"
 				/>
@@ -99,13 +113,14 @@ const RecipeDescriptionFields = ({ register, control }) => {
 					<Controller
 						control={control}
 						name="category"
+						rules={{ required: "Please choose a category" }}
 						render={({ field: { onChange, value } }) => (
 							<StyledSelect
 								// {...field}
-								defaultValue={optionCategories[1]}
+								// defaultValue={optionCategories[1]}
 								options={optionCategories}
-								value={value}
-								onChange={onChange}
+								value={getValue(value, optionCategories)}
+								onChange={(newValue) => onChange(newValue.label)}
 								placeholder="Cocktail"
 								classNamePrefix={"select"}
 							/>
@@ -117,13 +132,14 @@ const RecipeDescriptionFields = ({ register, control }) => {
 					<Controller
 						control={control}
 						name="glass"
+						rules={{ required: "Please choose a glass" }}
 						render={({ field: { onChange, value } }) => (
 							<StyledSelect
 								// {...field}
-								defaultValue={glasses[1]}
+								// defaultValue={glasses[1]}
 								options={optionGlasses}
-								value={value}
-								onChange={onChange}
+								value={getValue(value, optionGlasses)}
+								onChange={(newValue) => onChange(newValue.label)}
 								placeholder="Highball glass"
 								classNamePrefix={"select"}
 							/>
@@ -136,7 +152,9 @@ const RecipeDescriptionFields = ({ register, control }) => {
 };
 
 RecipeDescriptionFields.propTypes = {
-  register: PropTypes.func.isRequired,
+	register: PropTypes.func.isRequired,
+	getFile: PropTypes.func.isRequired,
+	control: PropTypes.shape({}).isRequired,
 };
 
 export default RecipeDescriptionFields;
