1d235c958e31342864ccfdf08b37cd7f1f0e79e8
Vladyslav Hrytsenko <unknownghost84.vg@gmail.com>
2023-08-29 15:04:27 +0300

    заглушки

---
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import RecipePageTitle from './RecipePageTitle';
+import AddToFavorite from './AddToFavorite';
+import RemoveFromFavorite from './RemoveFromFavorite';
+
+const RecipePageHero = ({ recipeData, isFavorite, onToggleFavorite }) => {
+  const [isInFavorite, setIsInFavorite] = useState(isFavorite);
+
+  const handleToggleFavorite = () => {
+    onToggleFavorite(recipeData.id);
+    setIsInFavorite(!isInFavorite);
+  };
+
+  return (
+    <div className="recipe-page-hero">
+      <RecipePageTitle title={recipeData.title} />
+      <p className="recipe-description">{recipeData.description}</p>
+      {isInFavorite ? (
+        <RemoveFromFavorite onClick={handleToggleFavorite} />
+      ) : (
+        <AddToFavorite onClick={handleToggleFavorite} />
+      )}
+      <p className="serving">{recipeData.serving}</p>
+      {/* Інші додаткові деталі */}
+    </div>
+  );
+};
+
+export default RecipePageHero;
