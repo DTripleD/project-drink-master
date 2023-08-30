ffb935fc9f740976591985d7785a7694b5a03fcb
Danil Deineka <tripledpadik@gmail.com>
2023-08-30 15:07:41 +0300

    bug fix

--- src/components/RecipePageHero/RecipePageHero.jsx
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -6,7 +6,9 @@ import MainPageTitle from "../MainPageTitle/MainPageTitle";
 // const RecipePageHero = ({ recipeData, isFavorite, onToggleFavorite }) => {
 
 // eslint-disable-next-line react/prop-types
-const RecipePageHero = ({ ingridients: { drink,glass,drinkThumb,description } }) => {
+const RecipePageHero = ({
+  ingridients: { drink, glass, drinkThumb, description },
+}) => {
   //   const [isInFavorite, setIsInFavorite] = useState(isFavorite);
 
   //   const handleToggleFavorite = () => {
@@ -18,7 +20,13 @@ const RecipePageHero = ({ ingridients: { drink,glass,drinkThumb,description } })
     <div className="recipe-page-hero">
       <p>{glass}</p>
       <MainPageTitle title={drink} />
-      <p>{description}</p>
+      <p>
+        {description} || Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Praesentium debitis optio deserunt dignissimos commodi nisi
+        expedita itaque dolor provident. Atque labore, corrupti totam
+        accusantium omnis alias vero. Itaque eos ullam aut rerum quam. Non
+        temporibus vel dolore similique, suscipit amet.
+      </p>
       <img src={drinkThumb} alt={drink} loading="lazy" />
       <button>Add to favourite</button>
     </div>
