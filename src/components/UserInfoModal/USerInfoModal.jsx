76f105b07e1f1251b82b0445c18b6af39fb22700
Danil Deineka <tripledpadik@gmail.com>
2023-08-31 13:08:07 +0300

    Bug fix

--- src/components/UserInfoModal/USerInfoModal.jsx
+++ src/components/UserInfoModal/USerInfoModal.jsx
@@ -6,110 +6,110 @@ import { selectUser } from "../../redux/auth/selectors";
 // import { ReactComponent as editSVG } from "../images/svg/edit.svg";
 
 import { BackDrop, Modal, CloseBtn } from "./UserInfoModal.styled";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const modalRoot = document.querySelector("#modal-root");
 // const StyledCloseIcon = getStyledCloseIcon(CloseMenuIcon);
 
 export const UserInfoModal = ({ handleModalClose }) => {
-	const dispatch = useDispatch();
-	const { name, avatarURL } = useSelector(selectUser);
-	const [image, setImage] = useState({ preview: avatarURL, data: null });
-	const [userName, setUserName] = useState(name);
-	const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+  const dispatch = useDispatch();
+  const { name, avatarURL } = useSelector(selectUser);
+  const [image, setImage] = useState({ preview: avatarURL, data: null });
+  const [userName, setUserName] = useState(name);
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
 
-	const userInfoFormSubmit = (e) => {
-		e.preventDefault();
+  const userInfoFormSubmit = (e) => {
+    e.preventDefault();
 
-		if (userName === name && !image.data) {
-			setIsButtonEnabled(false);
+    if (userName === name && !image.data) {
+      setIsButtonEnabled(false);
 
-			return;
-		}
-		const updatedUserName = e.target.elements.user_name.value;
-		setUserName(updatedUserName);
+      return;
+    }
+    const updatedUserName = e.target.elements.user_name.value;
+    setUserName(updatedUserName);
 
-		const formData = new FormData();
-		formData.append("file", image.data);
-		formData.append("userName", userName);
+    const formData = new FormData();
+    formData.append("file", image.data);
+    formData.append("userName", userName);
 
-		dispatch(update(formData));
+    dispatch(update(formData));
 
-		handlerEditProfileClick();
-	};
+    handlerEditProfileClick();
+  };
 
-	const onImageChange = (e) => {
-		const img = {
-			preview: URL.createObjectURL(e.target.files[0]),
-			data: e.target.files[0],
-		};
-		setImage(img);
-		setIsButtonEnabled(true);
-	};
+  const onImageChange = (e) => {
+    const img = {
+      preview: URL.createObjectURL(e.target.files[0]),
+      data: e.target.files[0],
+    };
+    setImage(img);
+    setIsButtonEnabled(true);
+  };
 
-	const onNameChange = (e) => {
-		if (userName !== e.target.value) {
-			setIsButtonEnabled(true);
-		}
-	};
+  const onNameChange = (e) => {
+    if (userName !== e.target.value) {
+      setIsButtonEnabled(true);
+    }
+  };
 
-	useEffect(() => {
-		if (userName !== name) {
-			setIsButtonEnabled(true);
-		}
-	}, [userName, name]);
+  useEffect(() => {
+    if (userName !== name) {
+      setIsButtonEnabled(true);
+    }
+  }, [userName, name]);
 
-	useEffect(() => {
-		const userImage = document.getElementById("user_image");
-		if (image.preview) {
-			userImage.src = image.preview;
-		}
-		return () => {
-			if (image.preview) {
-				URL.revokeObjectURL(image.preview);
-			}
-		};
-	}, [image.preview]);
+  useEffect(() => {
+    const userImage = document.getElementById("user_image");
+    if (image.preview) {
+      userImage.src = image.preview;
+    }
+    return () => {
+      if (image.preview) {
+        URL.revokeObjectURL(image.preview);
+      }
+    };
+  }, [image.preview]);
 
-	useEffect(() => {
-		const handleKeyDown = (event) => {
-			if (event.code === "Escape") {
-				handleModalClose();
-			}
-		};
-		window.addEventListener("keydown", handleKeyDown);
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.code === "Escape") {
+        handleModalClose();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
 
-		return () => {
-			window.removeEventListener("keydown", handleKeyDown);
-		};
-	}, [handleModalClose]);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleModalClose]);
 
-	const handleBackdropClick = (event) => {
-		if (event.currentTarget === event.target) {
-			handleModalClose();
-		}
-	};
+  const handleBackdropClick = (event) => {
+    if (event.currentTarget === event.target) {
+      handleModalClose();
+    }
+  };
 
-	const handleUploadClick = (event) => {
-		const file = event.target.files[0];
+  const handleUploadClick = (event) => {
+    const file = event.target.files[0];
 
-		const reader = new FileReader();
-		if (file) {
-			reader.readAsDataURL(file);
-			reader.onloadend = () => {
-				setImage(reader.result);
-				getFile(file);
-			};
-		}
-	};
+    const reader = new FileReader();
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setImage(reader.result);
+        getFile(file);
+      };
+    }
+  };
 
-	return createPortal(
-		<BackDrop onClick={handleBackdropClick}>
-			<Modal>
-				<CloseBtn type="button">{Cross}</CloseBtn>
-				<form action=""></form>
-			</Modal>
-		</BackDrop>,
-		modalRoot
-	);
+  return createPortal(
+    <BackDrop onClick={handleBackdropClick}>
+      <Modal>
+        <CloseBtn type="button">{Cross}</CloseBtn>
+        <form action=""></form>
+      </Modal>
+    </BackDrop>,
+    modalRoot
+  );
 };
--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -3,11 +3,11 @@ import { useDispatch } from "react-redux";
 import { createPortal } from "react-dom";
 import { ReactComponent as editSVG } from "../../images/svg/edit.svg";
 import {
-	getStyledEdit,
-	Drop,
-	EditButton,
-	SvgButton,
-	LogOutButton,
+  getStyledEdit,
+  Drop,
+  EditButton,
+  SvgButton,
+  LogOutButton,
 } from "./UserLogoModal.styled";
 import { logout } from "../../redux/auth/operations";
 import { useNavigate } from "react-router";
@@ -17,52 +17,52 @@ import { UserInfoModal } from "../UserInfoModal/USerInfoModal";
 const StyledEditSvg = getStyledEdit(editSVG);
 
 export const UserLogoModal = ({ setOpenDrop }) => {
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	const [showInfoModal, setShowInfoModal] = useState(false);
-	const [showCloseModal, setShowCloseModal] = useState(false);
+  const [showInfoModal, setShowInfoModal] = useState(false);
+  const [showCloseModal, setShowCloseModal] = useState(false);
 
-	const handleCloseModalOpen = () => {
-		setShowCloseModal(true);
-	};
+  const handleCloseModalOpen = () => {
+    setShowCloseModal(true);
+  };
 
-	const handleInfoModalOpen = () => {
-		setShowInfoModal(true);
-	};
+  const handleInfoModalOpen = () => {
+    setShowInfoModal(true);
+  };
 
-	const handleModalClose = () => {
-		setShowInfoModal(false);
-		setShowCloseModal(false);
-	};
+  const handleModalClose = () => {
+    setShowInfoModal(false);
+    setShowCloseModal(false);
+  };
 
-	const handleLogout = (event) => {
-		event.preventDefault();
-		dispatch(logout());
-		navigate("/welcome");
-	};
+  const handleLogout = (event) => {
+    event.preventDefault();
+    dispatch(logout());
+    navigate("/welcome");
+  };
 
-	return (
-		<>
-			<Drop open={setOpenDrop}>
-				<div>
-					<EditButton onClick={handleInfoModalOpen} type="button">
-						Edit profile
-					</EditButton>
-					<SvgButton>{<StyledEditSvg />}</SvgButton>
-				</div>
-				<LogOutButton onClick={handleCloseModalOpen} type="button">
-					Log out
-				</LogOutButton>
-			</Drop>
+  return (
+    <>
+      <Drop open={setOpenDrop}>
+        <div>
+          <EditButton onClick={handleInfoModalOpen} type="button">
+            Edit profile
+          </EditButton>
+          <SvgButton>{<StyledEditSvg />}</SvgButton>
+        </div>
+        <LogOutButton onClick={handleCloseModalOpen} type="button">
+          Log out
+        </LogOutButton>
+      </Drop>
 
-			{showInfoModal && (
-				<UserInfoModal handleInfoModalOpen={handleInfoModalOpen} />
-			)}
+      {showInfoModal && (
+        <UserInfoModal handleInfoModalOpen={handleInfoModalOpen} />
+      )}
 
-			{showCloseModal && (
-				<LogoutBtn handleInfoModalOpen={handleInfoModalOpen} />
-			)}
-		</>
-	);
+      {showCloseModal && (
+        <LogoutBtn handleInfoModalOpen={handleInfoModalOpen} />
+      )}
+    </>
+  );
 };
