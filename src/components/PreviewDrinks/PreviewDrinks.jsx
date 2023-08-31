fda73d1c130656fb67a2f64ab5adbba50a83b8fb
andreas7500 <andreas2875@gmail.com>
2023-08-31 18:23:47 +0300

    fix style main_page

--- src/components/PreviewDrinks/PreviewDrinks.jsx
+++ src/components/PreviewDrinks/PreviewDrinks.jsx
@@ -4,47 +4,47 @@ import PropTypes from "prop-types";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import { DrinkCard } from "../DrinkCard/DrinkCard";
-import { Container } from "../Container/Container";
+// import { Container } from "../Container/Container";
 import { Cardwrapper, Section, SectionTitle } from "./PreviewDrinks.styled";
 
 export const PreviewDrinks = ({ title, data = [] }) => {
   return (
     <Section>
-      <Container>
-        <Link to={`/drinks`} state={{ category: title }}>
-          <SectionTitle>{title}</SectionTitle>
-        </Link>
-        <Cardwrapper>
-          <Swiper
-            loop={true} // infinite loop
-            autoplay={{
-              delay: 2000,
-              disableOnInteraction: false,
-            }}
-            modules={[Autoplay, Pagination, Navigation]}
-            breakpoints={{
-              320: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              1440: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-              },
-            }}
-          >
-            {data.map((cocktail) => (
-              <SwiperSlide key={cocktail._id}>
-                <DrinkCard cocktail={cocktail} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </Cardwrapper>
-      </Container>
+      {/* <Container> */}
+      <Link to={`/drinks`} state={{ category: title }}>
+        <SectionTitle>{title}</SectionTitle>
+      </Link>
+      <Cardwrapper>
+        <Swiper
+          loop={true} // infinite loop
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Pagination, Navigation]}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1440: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+          }}
+        >
+          {data.map((cocktail) => (
+            <SwiperSlide key={cocktail._id}>
+              <DrinkCard cocktail={cocktail} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Cardwrapper>
+      {/* </Container> */}
     </Section>
   );
 };
