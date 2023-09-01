5f479f43886ef14f548f61aa2f2c69cbd8106c11
Danil Deineka <tripledpadik@gmail.com>
2023-09-02 00:13:02 +0300

    Bug fix

---
+++ src/shared/toasterOptions/toasterOptions.js
@@ -0,0 +1,20 @@
+export const toastOptions = {
+  success: {
+    style: {
+      background: "#66BB6A",
+      border: "1px solid ##66BB6A",
+      borderRadius: "4px",
+      color: "white",
+    },
+    icon: null,
+  },
+  error: {
+    style: {
+      background: "#E57373",
+      border: "1px solid #FF5733",
+      borderRadius: "4px",
+      color: "white",
+    },
+    icon: null,
+  },
+};
