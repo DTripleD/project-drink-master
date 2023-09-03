87189bc5063426e63741511c94d722c4f7e6cf3b
Danil Deineka <tripledpadik@gmail.com>
2023-09-03 15:00:05 +0300

    Bug fix

--- src/components/Header/BurgerMenu/BurgerMenu.jsx
+++ src/components/Header/BurgerMenu/BurgerMenu.jsx
@@ -2,11 +2,11 @@ import PropTypes from "prop-types";
 import { ReactComponent as OpenMenuIcon } from "../../../images/svg/align-justify.svg";
 import { ReactComponent as CloseMenuIcon } from "../../../images/svg/close.svg";
 import {
-	StyledMenu,
-	StyledMenuBtn,
-	StyledBurger,
-	getStyledOpenIcon,
-	getStyledCloseIcon,
+  StyledMenu,
+  StyledMenuBtn,
+  StyledBurger,
+  getStyledOpenIcon,
+  getStyledCloseIcon,
 } from "./BurgerMenu.styled";
 
 import { Navigation } from "../Navigation/Navigation";
@@ -17,36 +17,36 @@ const StyledOpenIcon = getStyledOpenIcon(OpenMenuIcon);
 const StyledCloseIcon = getStyledCloseIcon(CloseMenuIcon);
 
 export const Menu = ({ open, setOpen }) => {
-	const location = useLocation();
+  const location = useLocation();
 
-	useEffect(() => {
-		setOpen(false);
-	}, [location.pathname]);
+  useEffect(() => {
+    setOpen(false);
+  }, [location.pathname]);
 
-	return (
-		<StyledMenu open={open}>
-			<StyledMenuBtn type="button" onClick={() => setOpen(!open)}>
-				{<StyledCloseIcon />}
-			</StyledMenuBtn>
-			<Navigation />
-		</StyledMenu>
-	);
+  return (
+    <StyledMenu open={open}>
+      <StyledMenuBtn type="button" onClick={() => setOpen(!open)}>
+        {<StyledCloseIcon />}
+      </StyledMenuBtn>
+      <Navigation />
+    </StyledMenu>
+  );
 };
 
 export const Burger = ({ open, setOpen }) => {
-	return (
-		<StyledBurger open={open} onClick={() => setOpen(!open)}>
-			{<StyledOpenIcon />}
-		</StyledBurger>
-	);
+  return (
+    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      {<StyledOpenIcon />}
+    </StyledBurger>
+  );
 };
 
 Burger.propTypes = {
-	open: PropTypes.bool,
-	setOpen: PropTypes.func,
+  open: PropTypes.bool,
+  setOpen: PropTypes.func,
 };
 
 Menu.propTypes = {
-	open: PropTypes.bool,
-	setOpen: PropTypes.func,
+  open: PropTypes.bool,
+  setOpen: PropTypes.func,
 };
