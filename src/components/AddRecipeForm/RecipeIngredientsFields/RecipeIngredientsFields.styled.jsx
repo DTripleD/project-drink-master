d38c57ad42d99a0353014fc9b9ad81364649e17f
MargaritaSukhova <margarytasu@gmail.com>
2023-09-06 00:27:52 +0300

    react hook form domination :)

--- src/components/AddRecipeForm/RecipeIngredientsFields/RecipeIngredientsFields.styled.jsx
+++ src/components/AddRecipeForm/RecipeIngredientsFields/RecipeIngredientsFields.styled.jsx
@@ -145,6 +145,9 @@ export const SelectContainer = styled.div`
     padding: 14px 24px;
   }
 `;
+export const ErrorContainer = styled.div`
+	position: relative;
+`;
 
 export const Error = styled.p`
   color: red;
@@ -155,168 +158,165 @@ export const Error = styled.p`
 `;
 
 export const StyledSelect = styled(Select)`
-  .select__menu {
-    background-color: ${({ theme }) =>
-      theme.currentTheme === "light"
-        ? theme.colors.lightBlue
-        : theme.colors.blue};
-    border-radius: 20px;
-    outline: 0;
-    border: 0;
-    margin-top: -5px;
-  }
+	.select__menu {
+		background-color: ${({ theme }) =>
+			theme.currentTheme === "light"
+				? theme.colors.lightBlue
+				: theme.colors.blue};
+		border-radius: 20px;
+		outline: 0;
+		border: 0;
+		margin-top: -5px;
+	}
 
-  .select__menu:hover {
-    outline: 0;
-  }
+	.select__menu:hover {
+		outline: 0;
+	}
 
-  .select__menu-list {
-    padding: 18px;
-    max-height: 185px;
-    scrollbar-width: 4px;
-    overflow-x: hidden;
-  }
+	.select__menu-list {
+		padding: 18px;
+		max-height: 185px;
+		scrollbar-width: 4px;
+		overflow-x: hidden;
+	}
 
-  .select__value-container {
-    padding: 0;
-    text-align: left;
-  }
+	.select__value-container {
+		padding: 0;
+		text-align: left;
+	}
 
-  .select__control {
-    cursor: pointer;
-    padding: 16px 18px;
-    border-radius: 200px;
-    border: 1px solid
-      ${({ theme }) =>
-        theme.currentTheme === "light"
-          ? theme.colors.white
-          : theme.colors.lightGrey};
-    opacity: 0.8;
-    position: relative;
-    width: 200px;
-    height: 50px;
-    background-color: transparent;
-    color: ${(props) => props.theme.colors.white};
-    font-family: Manrope;
-    font-size: 14px;
-    @media (min-width: 768px) {
-      padding: 14px 24px;
-      width: 332px;
-      height: 56px;
-      font-size: 17px;
-      line-height: 156%;
-    }
-    :hover {
-      html,
-      body {
-        overflow: hidden;
-      }
-    }
-    :focus {
-      html,
-      body {
-        overflow: hidden;
-      }
-    }
-  }
+	.select__control {
+		cursor: pointer;
+		padding: 16px 18px;
+		border-radius: 200px;
+		border: 1px solid
+			${({ theme }) =>
+				theme.currentTheme === "light"
+					? theme.colors.white
+					: theme.colors.lightGrey};
+		opacity: 0.8;
+		position: relative;
+		width: 200px;
+		height: 50px;
+		background-color: transparent;
+		color: ${(props) => props.theme.colors.white};
+		font-family: Manrope;
+		font-size: 14px;
+		@media (min-width: 768px) {
+			padding: 14px 24px;
+			width: 332px;
+			height: 56px;
+			font-size: 17px;
+			line-height: 156%;
+		}
+		:hover {
+			html,
+			body {
+				overflow: hidden;
+			}
+		}
+		:focus {
+			html,
+			body {
+				overflow: hidden;
+			}
+		}
+	}
 
-  .select__indicator-separator {
-    display: none;
-  }
+	.select__indicator-separator {
+		display: none;
+	}
 
-  .select__input-container {
-    margin: 0;
-    padding: 0;
-    color: ${(props) => props.theme.colors.white};
-    font-family: Manrope;
-    font-size: 14px;
-    line-height: ${18 / 14}px;
-    @media (min-width: 768px) {
-      font-size: 17px;
-      line-height: 156%;
-    }
-  }
+	.select__input-container {
+		margin: 0;
+		padding: 0;
+		color: ${(props) => props.theme.colors.white};
+		font-family: Manrope;
+		font-size: 14px;
+		line-height: ${18 / 14}px;
+		@media (min-width: 768px) {
+			font-size: 17px;
+			line-height: 156%;
+		}
+	}
 
-  .select__input {
-    color: ${(props) => props.theme.colors.white};
-    font-family: Manrope;
-    font-size: 14px;
-    line-height: ${14 / 14}px;
-  }
+	.select__input {
+		color: ${(props) => props.theme.colors.white};
+		font-family: Manrope;
+		font-size: 14px;
+		line-height: ${14 / 14}px;
+	}
 
-  .select__placeholder {
-    position: absolute;
-    right: 0;
-    top: 3;
-    color: ${(props) => props.theme.colors.white};
-    font-family: Manrope;
-    font-size: 14px;
-    line-height: ${14 / 14}px;
-    @media (min-width: 768px) {
-      font-size: 17px;
-      line-height: 156%;
-    }
-  }
+	.select__placeholder {
+		color: ${(props) => props.theme.colors.white};
+		font-family: Manrope;
+		font-size: 14px;
+		line-height: ${14 / 14}px;
+		@media (min-width: 768px) {
+			font-size: 17px;
+			line-height: 156%;
+		}
+	}
 
-  .select__option {
-    background-color: transparent;
-    color: rgba(243, 243, 243, 0.4);
-    font-family: Manrope;
-    font-size: 12px;
-    line-height: ${16 / 10}px;
-    :hover {
-      color: ${(props) => props.theme.colors.white};
-    }
-    :focus {
-      color: ${(props) => props.theme.colors.white};
-    }
-    @media (min-width: 768px) {
-      font-size: 17px;
-      line-height: 156%;
-    }
-  }
+	.select__option {
+		background-color: transparent;
+		color: rgba(243, 243, 243, 0.4);
+		font-family: Manrope;
+		font-size: 12px;
+		line-height: ${16 / 10}px;
+		:hover {
+			color: ${(props) => props.theme.colors.white};
+		}
+		:focus {
+			color: ${(props) => props.theme.colors.white};
+		}
+		@media (min-width: 768px) {
+			font-size: 17px;
+			line-height: 156%;
+		}
+	}
 
-  .select__control--is-focused {
-    outline: 0;
-  }
+	.select__control--is-focused {
+		outline: 0;
+	}
 
-  .select__indicator {
-    padding: 0;
-    svg {
-      fill: ${({ theme }) =>
-        theme.currentTheme === "light"
-          ? theme.colors.white
-          : theme.colors.lightGrey};
-      transition: transform 300ms ease;
-      :hover {
-        fill: ${(props) => props.theme.colors.white};
-        transform: rotate(180deg);
-      }
-      :focus {
-        fill: ${(props) => props.theme.colors.white};
-        transform: rotate(180deg);
-      }
-      :active {
-        fill: ${(props) => props.theme.colors.white};
-        transform: rotate(180deg);
-      }
-    }
-  }
+	.select__indicator {
+		padding: 0;
+		svg {
+			fill: ${({ theme }) =>
+				theme.currentTheme === "light"
+					? theme.colors.white
+					: theme.colors.lightGrey};
+			transition: transform 300ms ease;
+			:hover {
+				fill: ${(props) => props.theme.colors.white};
+				transform: rotate(180deg);
+			}
+			:focus {
+				fill: ${(props) => props.theme.colors.white};
+				transform: rotate(180deg);
+			}
+			:active {
+				fill: ${(props) => props.theme.colors.white};
+				transform: rotate(180deg);
+			}
+		}
+	}
 
-  .select__value-container {
-    position: relative;
-  }
+	.select__value-container {
+		position: relative;
+	}
 
-  .select__single-value {
-    display: block;
-    color: ${(props) => props.theme.colors.white};
-    font-family: Manrope;
-    font-size: 14px;
-    @media (min-width: 768px) {
-      font-size: 17px;
-      line-height: 156%;
-    }
-  }
+	.select__single-value {
+		display: block;
+		color: ${(props) => props.theme.colors.white};
+		font-family: Manrope;
+		font-size: 14px;
+		@media (min-width: 768px) {
+			font-size: 17px;
+			line-height: 156%;
+		}
+	}
 `;
 
 export const StyledUnitSelect = styled(Select)`
