0c52b147a9a95f345ba82779693871ea910a364c
gulen13 <vitaliy.gulen@gmail.com>
2023-09-05 20:34:15 +0300

    update page

--- src/components/DrinksSearch/DrinksSearch.jsx
+++ src/components/DrinksSearch/DrinksSearch.jsx
@@ -1,17 +1,20 @@
-import { ReactComponent as Search } from "../../images/svg/search.svg";
-import DrinksList from "../../components/DrinksList/DrinksList";
 import { useLocation, useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { Controller } from "react-hook-form";
+//----------------Selectors---------------------
 import {
 	selectCategories,
 	selectIngredientsListSorted,
 } from "../../redux/drinks/drinksSelectors";
+//----------------Functions---------------------
 import {
 	getCategories,
 	getIngredients,
 } from "../../redux/drinks/drinksOperations";
+import { getDrinksList } from "../../shared/api/drinksSearch";
+//-----------------Styles------------------------
 import {
 	StyledSelect,
 	Form,
@@ -19,12 +22,13 @@ import {
 	InputContainer,
 	Input,
 } from "./DrinksSearch.styled";
-import { getDrinksList } from "../../shared/api/drinksSearch";
-import { useDispatch, useSelector } from "react-redux";
-import PaginationComponent from "../Pagination/Pagination";
 import { ErrorPageWrapper } from "../../pages/ErrorPage/ErrorPage.styled";
 import { P3 } from "../DrinksList/DrinksList.styled";
+//-----------------Components--------------------
+import PaginationComponent from "../Pagination/Pagination";
+import DrinksList from "../../components/DrinksList/DrinksList";
 import Loader from "../Loader/Loader";
+import { ReactComponent as Search } from "../../images/svg/search.svg";
 
 const DrinksSearch = () => {
 	const { state } = useLocation();
@@ -32,10 +36,8 @@ const DrinksSearch = () => {
 	const { register, handleSubmit, control } = useForm();
 	const [page, setPage] = useState(1);
 	const [totalPages, setTotalPages] = useState(1);
-
 	const [data, setData] = useState([]);
 	const [error, setError] = useState("");
-
 	const [itemsPerPage, setItemsPerPage] = useState(() => {
 		const width = window.innerWidth;
 
@@ -45,7 +47,6 @@ const DrinksSearch = () => {
 			return 10;
 		}
 	});
-
 	const [searchParams, setSearchParams] = useSearchParams({
 		search: "",
 		category: state?.category ? state?.category : "",
@@ -79,9 +80,6 @@ const DrinksSearch = () => {
 		return () => window.removeEventListener("resize", handleWindowResize);
 	}, []);
 
-	const categories = useSelector(selectCategories);
-	const ingredients = useSelector(selectIngredientsListSorted);
-
 	useEffect(() => {
 		const newPage = parseInt(searchParams.get("page")) || 1;
 		if (newPage !== page) {
@@ -98,6 +96,9 @@ const DrinksSearch = () => {
 				setError(error);
 			});
 	}, [searchParams]);
+
+	const categories = useSelector(selectCategories);
+	const ingredients = useSelector(selectIngredientsListSorted);
 
 	const optionCategories = categories.map((category) => ({
 		value: category,
