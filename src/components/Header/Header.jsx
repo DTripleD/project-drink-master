ec66e5ea3e1a775e08aa3cf47b39afa5fb56d7fa
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-09-07 00:53:57 +0300

    add chandes
    в бургерМеню виправила помилку
    в хедер додала ще один дів і стилі
    в інфоМодал видалила тостер
    в юзерМеню додала дів і стилі на ім*я  і в обгорці фото дописала стилі

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
-import { HeaderStyled, MenuWrepper } from "./Header.styled";
+import { HeaderStyled, MenuWrepper, UserMenuWrapper } from "./Header.styled";
 import WelcomePage from "../../pages/WelcomePage/WelcomePage";
 import { Logo } from "./Logo/Logo";
 import { Navigation } from "./Navigation/Navigation";
@@ -14,27 +14,28 @@ import { MainContainer } from "../MainContainer/MainContainer";
 import ThemeToggler from "../ThemeToggler/ThemeToggler";
 
 const Header = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const [open, setOpen] = useState(false);
+	const isLoggedIn = useSelector(selectIsLoggedIn);
+	const [open, setOpen] = useState(false);
 
-  return (
-    <MainContainer>
-      <HeaderStyled>
-        <Logo />
+	return (
+		<MainContainer>
+			<HeaderStyled>
+				<Logo />
 
-        <MediaQuery minWidth={1440}>{isLoggedIn && <Navigation />}</MediaQuery>
+				<MediaQuery minWidth={1440}>{isLoggedIn && <Navigation />}</MediaQuery>
 
-        <ThemeToggler />
+				<ThemeToggler />
+				<UserMenuWrapper>
+					<UserMenu />
 
-        <UserMenu />
-
-        <MenuWrepper>
-          <Burger open={open} setOpen={setOpen} />
-          <Menu open={open} setOpen={setOpen} />
-        </MenuWrepper>
-      </HeaderStyled>
-    </MainContainer>
-  );
+					<MenuWrepper>
+						<Burger open={open} setOpen={setOpen} />
+						<Menu open={open} setOpen={setOpen} />
+					</MenuWrepper>
+				</UserMenuWrapper>
+			</HeaderStyled>
+		</MainContainer>
+	);
 };
 
 export default Header;
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,24 +1,28 @@
 import styled from "@emotion/styled";
 
 export const HeaderStyled = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 20px 0;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 20px 0;
 
-  width: 100%;
-  box-sizing: border-box;
+	width: 100%;
+	box-sizing: border-box;
+`;
+
+export const UserMenuWrapper = styled.div`
+	display: flex;
+	gap: 14px;
+
+	@media (min-width: 768px) {
+		gap: 24px;
+	}
 `;
 
 export const MenuWrepper = styled.div`
-  // margin-left: 14px;
-  z-index: 800;
+	z-index: 800;
 
-  @media (min-width: 768px) {
-    margin-left: 24px;
-  }
-
-  @media (min-width: 1440px) {
-    display: none;
-  }
+	@media (min-width: 1440px) {
+		display: none;
+	}
 `;
