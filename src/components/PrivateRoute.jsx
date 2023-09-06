82dd3ca395d08f6577f7c9a43068b50a2cc0b6e4
Danil Deineka <tripledpadik@gmail.com>
2023-09-06 20:02:51 +0300

    Bug fix

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -6,8 +6,7 @@ import { selectIsLoggedIn, selectIsRefreshing } from "../redux/auth/selectors";
 const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
-  // const shouldRedirect = !isLoggedIn && !isRefreshing;
-  const shouldRedirect = false;
+  const shouldRedirect = !isLoggedIn && !isRefreshing;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -4,8 +4,7 @@ import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../redux/auth/selectors";
 
 const RestrictedRoute = ({ component: Component, redirectTo = "/" }) => {
-  // const isLoggedIn = useSelector(selectIsLoggedIn);
-  const isLoggedIn = true;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
