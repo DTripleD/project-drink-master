bfc1bf176192720a0566896847048e5da44d3d46
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-09-06 14:18:42 +0300

    return base_url

--- src/shared/api/instance.js
+++ src/shared/api/instance.js
@@ -4,8 +4,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: VITE_API_URL,
-  // baseURL: "https://drink-master-backend.onrender.com",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://drink-master-backend.onrender.com",
 });
 
 export default instance;
