7a9b2485d276bc5fd6ea107fb1c0ac46e756377d
Danil Deineka <tripledpadik@gmail.com>
2023-09-07 14:49:45 +0300

    bug fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,40 +2,38 @@ import { useSelector } from "react-redux";
 
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import { HeaderStyled, MenuWrepper, UserMenuWrapper } from "./Header.styled";
-import WelcomePage from "../../pages/WelcomePage/WelcomePage";
 import { Logo } from "./Logo/Logo";
 import { Navigation } from "./Navigation/Navigation";
 import UserMenu from "../UserMenu/UserMenu";
 import { Burger, Menu } from "./BurgerMenu/BurgerMenu";
 import MediaQuery from "react-responsive";
-import { useNavigate } from "react-router";
 import { useState } from "react";
 import { MainContainer } from "../MainContainer/MainContainer";
 import ThemeToggler from "../ThemeToggler/ThemeToggler";
 
 const Header = () => {
-	const isLoggedIn = useSelector(selectIsLoggedIn);
-	const [open, setOpen] = useState(false);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const [open, setOpen] = useState(false);
 
-	return (
-		<MainContainer>
-			<HeaderStyled>
-				<Logo />
+  return (
+    <MainContainer>
+      <HeaderStyled>
+        <Logo />
 
-				<MediaQuery minWidth={1440}>{isLoggedIn && <Navigation />}</MediaQuery>
+        <MediaQuery minWidth={1440}>{isLoggedIn && <Navigation />}</MediaQuery>
 
-				<ThemeToggler />
-				<UserMenuWrapper>
-					<UserMenu />
+        <ThemeToggler />
+        <UserMenuWrapper>
+          <UserMenu />
 
-					<MenuWrepper>
-						<Burger open={open} setOpen={setOpen} />
-						<Menu open={open} setOpen={setOpen} />
-					</MenuWrepper>
-				</UserMenuWrapper>
-			</HeaderStyled>
-		</MainContainer>
-	);
+          <MenuWrepper>
+            <Burger open={open} setOpen={setOpen} />
+            <Menu open={open} setOpen={setOpen} />
+          </MenuWrepper>
+        </UserMenuWrapper>
+      </HeaderStyled>
+    </MainContainer>
+  );
 };
 
 export default Header;
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -7,141 +7,155 @@ import { UserInfoModal } from "../UserInfoModal/UserInfoModal";
 import { useOnClickOutside } from "../../hooks/useOnClickOutside";
 import User from "../../images/user.png";
 
-import { selectUser } from "../../redux/auth/selectors";
+import { selectIsChanging, selectUser } from "../../redux/auth/selectors";
 import {
-	MenuWrapper,
-	UserName,
-	NameWrapper,
-	UserPhoto,
-	UserPhotoWrapper,
+  MenuWrapper,
+  UserName,
+  NameWrapper,
+  UserPhoto,
+  UserPhotoWrapper,
 } from "./UserMenu.styled";
 import { UserLogoModal } from "../UserLogoModal/UserLogoModal";
+import { Circles } from "react-loader-spinner";
 
 const UserMenu = () => {
-	const { name, avatarURL } = useSelector(selectUser);
+  const { name, avatarURL } = useSelector(selectUser);
+  const isChanging = useSelector(selectIsChanging);
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const [openDrop, setOpenDrop] = useState(false);
-	const [showInfoModal, setShowInfoModal] = useState(false);
-	const [showLogoutModal, setShowLogoutModal] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [openDrop, setOpenDrop] = useState(false);
+  const [showInfoModal, setShowInfoModal] = useState(false);
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
 
-	const node = useRef();
-	useOnClickOutside(node, () => setOpenDrop(false));
+  const node = useRef();
+  useOnClickOutside(node, () => setOpenDrop(false));
 
-	const handleLogoutModalOpen = () => {
-		setShowLogoutModal(true);
-		setOpenDrop(false);
-		document.body.style.overflow = "hidden";
-	};
+  const handleLogoutModalOpen = () => {
+    setShowLogoutModal(true);
+    setOpenDrop(false);
+    document.body.style.overflow = "hidden";
+  };
 
-	const handleInfoModalOpen = () => {
-		setShowInfoModal(true);
-		setOpenDrop(false);
-		document.body.style.overflow = "hidden";
-	};
+  const handleInfoModalOpen = () => {
+    setShowInfoModal(true);
+    setOpenDrop(false);
+    document.body.style.overflow = "hidden";
+  };
 
-	const handleModalClose = () => {
-		setShowInfoModal(false);
-		setShowLogoutModal(false);
-		setOpenDrop(false);
-		document.body.style.overflow = "";
-	};
+  const handleModalClose = () => {
+    setShowInfoModal(false);
+    setShowLogoutModal(false);
+    setOpenDrop(false);
+    document.body.style.overflow = "";
+  };
 
-	const handleLogout = (event) => {
-		dispatch(logout());
-		navigate("/");
-	};
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
 
-	const handleBackdropClick = (event) => {
-		if (event.currentTarget === event.target) {
-			handleModalClose();
-			document.body.style.overflow = "";
-		}
-	};
+  const handleBackdropClick = (event) => {
+    if (event.currentTarget === event.target) {
+      handleModalClose();
+      document.body.style.overflow = "";
+    }
+  };
 
-	const handleKeyDown = (event) => {
-		if (event.code === "Escape") {
-			handleModalClose();
-			document.body.style.overflow = "";
-		}
-	};
+  const handleKeyDown = (event) => {
+    if (event.code === "Escape") {
+      handleModalClose();
+      document.body.style.overflow = "";
+    }
+  };
 
-	useEffect(() => {
-		if (openDrop) {
-			window.addEventListener("keydown", handleKeyDown);
-			window.addEventListener("click", handleBackdropClick);
-		}
+  useEffect(() => {
+    if (openDrop) {
+      window.addEventListener("keydown", handleKeyDown);
+      window.addEventListener("click", handleBackdropClick);
+    }
 
-		return () => {
-			window.removeEventListener("keydown", handleKeyDown);
-			window.removeEventListener("click", handleBackdropClick);
-		};
-	}, [handleModalClose]);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("click", handleBackdropClick);
+    };
+  }, [handleModalClose]);
 
-	useEffect(() => {
-		if (showInfoModal) {
-			window.addEventListener("keydown", handleKeyDown);
-			window.addEventListener("click", handleBackdropClick);
-		}
+  useEffect(() => {
+    if (showInfoModal) {
+      window.addEventListener("keydown", handleKeyDown);
+      window.addEventListener("click", handleBackdropClick);
+    }
 
-		return () => {
-			window.removeEventListener("keydown", handleKeyDown);
-			window.removeEventListener("click", handleBackdropClick);
-		};
-	}, [handleModalClose]);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("click", handleBackdropClick);
+    };
+  }, [handleModalClose]);
 
-	useEffect(() => {
-		if (showLogoutModal) {
-			window.addEventListener("keydown", handleKeyDown);
-			window.addEventListener("click", handleBackdropClick);
-		}
+  useEffect(() => {
+    if (showLogoutModal) {
+      window.addEventListener("keydown", handleKeyDown);
+      window.addEventListener("click", handleBackdropClick);
+    }
 
-		return () => {
-			window.removeEventListener("keydown", handleKeyDown);
-			window.removeEventListener("click", handleBackdropClick);
-		};
-	}, [handleModalClose]);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("click", handleBackdropClick);
+    };
+  }, [handleModalClose]);
 
-	return (
-		<>
-			<MenuWrapper open={openDrop} onClick={() => setOpenDrop(!openDrop)}>
-				<UserPhotoWrapper>
-					<UserPhoto src={avatarURL || User} alt="" />
-				</UserPhotoWrapper>
-				<NameWrapper>
-					<UserName>{name}</UserName>
-				</NameWrapper>
+  return (
+    <>
+      <MenuWrapper open={openDrop} onClick={() => setOpenDrop(!openDrop)}>
+        <UserPhotoWrapper>
+          {isChanging ? (
+            <Circles
+              height="35"
+              width="35"
+              color="#256294"
+              ariaLabel="circles-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          ) : (
+            <UserPhoto src={avatarURL || User} alt="" />
+          )}
+        </UserPhotoWrapper>
+        <NameWrapper>
+          <UserName>{name}</UserName>
+        </NameWrapper>
 
-				{openDrop && (
-					<div ref={node}>
-						<UserLogoModal
-							handleInfoModalOpen={handleInfoModalOpen}
-							handleLogoutModalOpen={handleLogoutModalOpen}
-						/>
-					</div>
-				)}
-			</MenuWrapper>
+        {openDrop && (
+          <div ref={node}>
+            <UserLogoModal
+              handleInfoModalOpen={handleInfoModalOpen}
+              handleLogoutModalOpen={handleLogoutModalOpen}
+            />
+          </div>
+        )}
+      </MenuWrapper>
 
-			{showInfoModal && (
-				<UserInfoModal
-					handleInfoModalOpen={handleInfoModalOpen}
-					handleBackdropClick={handleBackdropClick}
-					handleLogoutModalOpen={handleLogoutModalOpen}
-					handleModalClose={handleModalClose}
-				/>
-			)}
+      {showInfoModal && (
+        <UserInfoModal
+          handleInfoModalOpen={handleInfoModalOpen}
+          handleBackdropClick={handleBackdropClick}
+          handleLogoutModalOpen={handleLogoutModalOpen}
+          handleModalClose={handleModalClose}
+        />
+      )}
 
-			{showLogoutModal && (
-				<LogoutBtn
-					handleInfoModalOpen={handleInfoModalOpen}
-					handleModalClose={handleModalClose}
-					handleLogout={handleLogout}
-					handleBackdropClick={handleBackdropClick}
-				/>
-			)}
-		</>
-	);
+      {showLogoutModal && (
+        <LogoutBtn
+          handleInfoModalOpen={handleInfoModalOpen}
+          handleModalClose={handleModalClose}
+          handleLogout={handleLogout}
+          handleBackdropClick={handleBackdropClick}
+        />
+      )}
+    </>
+  );
 };
 
 export default UserMenu;
