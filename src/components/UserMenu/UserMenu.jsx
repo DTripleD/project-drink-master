b75350fa97813a059297ba91498a0e192372ea41
Danil Deineka <tripledpadik@gmail.com>
2023-09-07 16:42:02 +0300

    Bug fix

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,7 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState, useRef } from "react";
 import { logout } from "../../redux/auth/operations";
-import { useNavigate } from "react-router";
 import { LogoutBtn } from "../LogoutBtn/LogoutBtn";
 import { UserInfoModal } from "../UserInfoModal/UserInfoModal";
 import { useOnClickOutside } from "../../hooks/useOnClickOutside";
@@ -19,12 +18,10 @@ import { UserLogoModal } from "../UserLogoModal/UserLogoModal";
 import { Circles } from "react-loader-spinner";
 
 const UserMenu = () => {
-
   const { name, avatarURL } = useSelector(selectUser);
   const isChanging = useSelector(selectIsChanging);
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const [openDrop, setOpenDrop] = useState(false);
   const [showInfoModal, setShowInfoModal] = useState(false);
   const [showLogoutModal, setShowLogoutModal] = useState(false);
@@ -53,7 +50,6 @@ const UserMenu = () => {
 
   const handleLogout = () => {
     dispatch(logout());
-    navigate("/");
   };
 
   const handleBackdropClick = (event) => {
